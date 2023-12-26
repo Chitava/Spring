badc163863a1093b3a58cf489160ad92ec5f4d31
Читава Александр Валикоевич <chitava@lsin.ln>
2023-12-26 09:57:13 +0300

    Начал серриализацию gson

--- SpringHW1/src/main/java/Main.java
+++ SpringHW1/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
         Person persona1 = new Person("Смирнов", "Сергей", 23);
@@ -19,6 +22,10 @@ public class Main {
             System.out.println("Вы выбрали разных пользователей");
         }
         System.out.println(persona1.hashCode());
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(persona1);
+        System.out.println(json);
 
 
     }
