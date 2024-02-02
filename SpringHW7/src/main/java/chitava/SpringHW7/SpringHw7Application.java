18b5b393c107af4039c5e49389cbb7bca748ab6c
Читава Александр Валикоевич <chitava@lsin.ln>
2024-02-02 11:01:09 +0300

    Выполнил первую задачу из ДЗ №7

---
+++ SpringHW7/src/main/java/chitava/SpringHW7/SpringHw7Application.java
@@ -0,0 +1,17 @@
+package chitava.SpringHW7;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.userdetails.User;
+
+@SpringBootApplication
+
+public class SpringHw7Application {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw7Application.class, args);
+	}
+
+}
