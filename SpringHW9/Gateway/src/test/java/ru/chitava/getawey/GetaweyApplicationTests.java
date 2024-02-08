335963bd6615bd9cbbc3b4b74f87d5ccf33795a3
Читава Александр Валикоевич <chitava@lsin.ln>
2024-02-08 11:18:26 +0300

    Начал дз №9

---
+++ SpringHW9/Gateway/src/test/java/ru/chitava/getawey/GetaweyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chitava.getawey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetaweyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
